2dfd7b526848edabf73268767f993baf14113554
João Vitor CF <joaovitormgouveia@outlook.com>
2023-07-07 22:45:41 -0300

    PC-CF comentários até aula 2 capítulo 2

--- src/util/errors/internal-error.ts
+++ src/util/errors/internal-error.ts
@@ -1,7 +1,7 @@
-export class InternalError extends Error {
+export class InternalError extends Error { // Erro interno do nosso servidor, o usuário não irá vê-lo
     constructor(public message: string, protected code: number = 500, protected description?: string){
-        super(message);
+        super(message); 
         this.name = this.constructor.name;
-        Error.captureStackTrace(this, this.constructor);
+        Error.captureStackTrace(this, this.constructor); // Joga a classe no "lixo" e mostra a partir de onde o erro foi chamado
     }
 }
